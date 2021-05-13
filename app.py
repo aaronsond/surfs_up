2053dcc5256281987e146db8bb82fae2b9654df7
David Aaronson <aaronsond@gmail.com>
2021-05-13 05:07:17 -0700

    minor commit update for next mod

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# 9.5.1
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+
 app = Flask(__name__)
 
 @app.route('/')
